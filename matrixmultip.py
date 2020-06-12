fcc074a57e1349415b8e324b3b095cbee4531da2
faruk-io <61500094+faruk-io@users.noreply.github.com>
2020-06-12 21:39:44 +0600

    matrix in python by using numpy
    addition,substraction & multipication

---
+++ matrixmultip.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+
+m1 =([3,2,1],
+     [4,5,6],
+     [9,7,8]
+     )
+
+# 3*1 + 2*9 + 1*4
+# #3+18+4
+# 25
+m2 =([1,2,3],
+     [9,5,5],
+     [4,7,8]
+     )
+result = np.add(m1 , m2)
+result = np.subtract(m1 , m2)
+result = np.dot(m1 , m2)
